08c51236ec517c4b00523782a7505764e98afd00
amitypreetiai-rgb <amitypreetiai@gmail.com>
2026-08-16 19:56:09 +0530

    Create Q13.java

---
+++ src/Module1/Q13.java
@@ -0,0 +1,25 @@
+package Module1;
+
+public class Q13 {
+    byte b;
+    short s;
+    int i;
+    long l;
+    float f;
+    double d;
+    char c;
+    boolean bool;
+
+    public static void main(String[] args) {
+        Q13 obj = new Q13();
+
+        System.out.println("byte = " + obj.b);
+        System.out.println("short = " + obj.s);
+        System.out.println("int = " + obj.i);
+        System.out.println("long = " + obj.l);
+        System.out.println("float = " + obj.f);
+        System.out.println("double = " + obj.d);
+        System.out.println("char = [" + obj.c + "]");
+        System.out.println("boolean = " + obj.bool);
+    }
+}
