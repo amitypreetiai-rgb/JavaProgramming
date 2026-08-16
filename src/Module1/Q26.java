07214224fd835bd4e2e851d7a59af4481c1f2bd4
amitypreetiai-rgb <amitypreetiai@gmail.com>
2026-08-16 18:30:08 +0530

    Create Q26.java

---
+++ src/Module1/Q26.java
@@ -0,0 +1,19 @@
+package Module1;
+
+import java.util.Arrays;
+
+public class Q26 {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+
+        System.out.println("Original array: " + Arrays.toString(arr));
+
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
+
+        System.out.println("Reversed array: " + Arrays.toString(arr));
+    }
+}
