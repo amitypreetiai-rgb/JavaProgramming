674f472dbdb992c42cc7930b73e2dffad04602fb
amitypreetiai-rgb <amitypreetiai@gmail.com>
2026-08-16 18:29:01 +0530

    Create Q25.java

---
+++ src/Module1/Q25.java
@@ -0,0 +1,15 @@
+package Module1;
+
+public class Q25 {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        System.out.println("Sum of array elements: " + sum);
+    }
+}
