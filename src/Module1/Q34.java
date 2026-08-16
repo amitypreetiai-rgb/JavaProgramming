be1d17191d96cc13e714905e83f5e4a94139f71a
amitypreetiai-rgb <amitypreetiai@gmail.com>
2026-08-16 18:32:34 +0530

    Create Q34.java

---
+++ src/Module1/Q34.java
@@ -0,0 +1,9 @@
+package Module1;
+
+public class Q34 {
+    public static void main(String[] args) {
+        int marks = 80;
+        String result = (marks >= 35) ? "PASS" : "FAIL";
+        System.out.println("Marks: " + marks + " = " + result);
+    }
+}
