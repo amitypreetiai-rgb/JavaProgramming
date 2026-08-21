20b62c034af89f8e25f9f77644392999e1e2761b
preeti <amitypreetiai@gmail.com>
2026-08-21 10:55:51 +0530

    Add array package

---
+++ src/Module2/functional/Calculator.java
@@ -0,0 +1,5 @@
+package Module2.functional;
+
+public interface Calculator {
+    int compute(int a, int b);
+}
---
+++ src/Module2/functional/CalculatorMain.java
@@ -0,0 +1,14 @@
+package Module2.functional;
+
+public class CalculatorMain {
+    public static void main(String[] args) {
+
+        Calculator addition = (a, b) -> a + b;
+        Calculator subtraction = (a, b) -> a - b;
+        Calculator multiplication = (a, b) -> a * b;
+
+        System.out.println("Addition: " + addition.compute(10, 5));
+        System.out.println("Subtraction: " + subtraction.compute(10, 5));
+        System.out.println("Multiplication: " + multiplication.compute(10, 5));
+    }
+}
